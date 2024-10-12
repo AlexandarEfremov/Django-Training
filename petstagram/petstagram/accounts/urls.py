13a082d5d702f323f7dd673dc0d458c365c23781
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-10-12 21:40:29 +0300

    added app specific urls

---
+++ petstagram/petstagram/accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+
+from petstagram.petstagram.accounts import views
+
+urlpatterns = (
+    path('register/', views.register, name="register"),
+    path('login/', views.login, name="login"),
+    path('profile/<int:pk>/', include([
+        path('', views.profile_details_page, name="profile-details-page"),
+        path('edit/', views.profile_edit_page, name="profile-edit-page"),
+        path('delete/', views.profile_delete_page, name="profile-delete-page")
+    ])),
+)
