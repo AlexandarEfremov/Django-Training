299e8b4efd658fd9caa00fe227d8296e3aee527b
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-10-13 15:17:11 +0300

    created Comment and Like models, init admin for both

--- petstagram/petstagram/common/admin.py
+++ petstagram/petstagram/common/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Like
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    pass
+
+
--- petstagram/petstagram/photos/admin.py
+++ petstagram/petstagram/photos/admin.py
@@ -5,4 +5,8 @@ from .models import Photo
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ["photo", "date_of_publication", "location"]
+    list_display = ["id", "date_of_publication", "description", "get_tagged_pets"]
+
+    @staticmethod
+    def get_tagged_pets(obj):
+        return ', '.join([pet.name for pet in obj.tagged_pets.all()])
