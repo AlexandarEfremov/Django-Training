f5e1c5d970717c3322d8e43dc54ba84463f45c0e
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-10-13 22:01:02 +0300

    added like button

--- petstagram/petstagram/common/urls.py
+++ petstagram/petstagram/common/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name="home-page")
+    path('', views.home_page, name="home-page"),
+    path('like/<int:photo_id>/', views.like_functionality, name="like")
 ]
--- petstagram/petstagram/common/views.py
+++ petstagram/petstagram/common/views.py
@@ -1,8 +1,28 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from ..photos.models import Photo
+from ..common.models import Like
 
 
 def home_page(request):
-    return render(request, template_name='common/home-page.html')
+    all_photos = Photo.objects.all()
+
+    context = {
+        "all_photos": all_photos
+    }
+
+    return render(request, template_name='common/home-page.html', context=context)
+
+
+def like_functionality(request, photo_id):
+    photo = Photo.objects.get(id=photo_id)
+    liked_object = Like.objects.filter(to_photo_id=photo).first()
+
+    if liked_object:
+        liked_object.delete()
+    else:
+        like = Like(to_photo=photo)
+        like.save()
+
+    return redirect(request.META['HTTP_REFERER'] + f"#{photo_id}")
