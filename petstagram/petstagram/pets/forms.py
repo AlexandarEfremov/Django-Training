9d56da7351f047c0c3cb9c38e8396b6ddbc22b29
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-10-14 17:05:01 +0300

    added pet form and customized it in the views.py

--- petstagram/petstagram/pets/forms.py
+++ petstagram/petstagram/pets/forms.py
@@ -6,3 +6,15 @@ class PetForm(forms.ModelForm):
     class Meta:
         model = Pet
         fields = ['name', 'date_of_birth', 'personal_photo']
+        widgets = {
+            "name": forms.TextInput(attrs={"placeholder": "Pet name"}),
+            "date_of_birth": forms.DateInput(attrs={"type": "date"}),
+            "personal_photo": forms.TextInput(attrs={"placeholder": "Link to image"}),
+        }
+
+        labels = {
+            "name": "Pet name",
+            "date_of_birth": "Date of Birth",
+            "personal_photo": "Link to Image",
+        }
+
