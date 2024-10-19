ca3b9ce6d5d4ec8c1e8e278bf5e83e8664529373
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-10-19 22:34:05 +0300

    implemented pagination

--- petstagram/petstagram/common/views.py
+++ petstagram/petstagram/common/views.py
@@ -1,29 +1,63 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, resolve_url
 from pyperclip import copy
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from ..photos.models import Photo
 from ..common.models import Like
 from ..common.forms import CommentForm, SearchForm
+from django.views.generic import ListView
 
 
-def home_page(request):
-    all_photos = Photo.objects.all()
-    comment_form = CommentForm()
-    search_form = SearchForm(request.GET)
+class HomePageView(ListView):
+    model = Photo
+    template_name = "common/home-page.html"
+    context_object_name = "page_obj"
+    paginate_by = 1
 
-    if search_form.is_valid():
-        all_photos = all_photos.filter(
-            tagged_pets__name__icontains=search_form.cleaned_data['pet_name']
-        )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["comment_form"] = CommentForm()
+        context["search_form"] = SearchForm(self.request.GET)
+        return context
 
-    context = {
-        "all_photos": all_photos,
-        "comment_form": comment_form,
-        "search_form": search_form
-    }
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        pet_name = self.request.GET.get("pet_name")
 
-    return render(request, template_name='common/home-page.html', context=context)
+        if pet_name:
+            queryset = queryset.filter(tagged_pets__name__icontains=pet_name)
+
+        return queryset
+
+# def home_page(request):
+#     all_photos = Photo.objects.all()
+#     comment_form = CommentForm()
+#     search_form = SearchForm(request.GET)
+#
+#     if search_form.is_valid():
+#         all_photos = all_photos.filter(
+#             tagged_pets__name__icontains=search_form.cleaned_data['pet_name']
+#         )
+#
+#     photos_per_page = 1
+#     paginator = Paginator(all_photos, photos_per_page)
+#     page = request.GET.get("page")
+#
+#     try:
+#         all_photos = paginator.page(page)
+#     except PageNotAnInteger:
+#         all_photos = paginator.page(1)
+#     except EmptyPage:
+#         all_photos = paginator.page(paginator.num_pages)
+#
+#
+#     context = {
+#         "all_photos": all_photos,
+#         "comment_form": comment_form,
+#         "search_form": search_form
+#     }
+#
+#     return render(request, template_name='common/home-page.html', context=context)
 
 
 def like_functionality(request, photo_id):
--- petstagram/petstagram/pets/urls.py
+++ petstagram/petstagram/pets/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('add/', views.AddPetView.as_view(), name="pet-add-page"),
     path('<str:username>/pet/<slug:pet_slug>/', include([
         path('', views.PetDetailsView.as_view(), name="pet-details-page"),
-        path('edit/', views.EditPetView, name="pet-edit-page"),
+        path('edit/', views.EditPetView.as_view(), name="pet-edit-page"),
         path('delete/', views.DeletePetView.as_view(), name="pet-delete-page")
     ])),
 ]
