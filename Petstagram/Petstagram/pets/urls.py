0c669767e9bab479c73855dc9f4163a3c5a9f959
Alex <efremov.alex@yahoo.bg>
2024-11-10 21:35:42 +0200

    Petstagram for Django Advanced

---
+++ Petstagram/Petstagram/pets/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    # path('add/', views.AddPetView.as_view(), name="pet-add-page"),
+    # path('<str:username>/pet/<slug:pet_slug>/', include([
+    #     path('', views.PetDetailsView.as_view(), name="pet-details-page"),
+    #     path('edit/', views.EditPetView.as_view(), name="pet-edit-page"),
+    #     path('delete/', views.DeletePetView.as_view(), name="pet-delete-page")
+    # ])),
+]
